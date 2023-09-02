61b612e03cd8df56b2487d2a923a0f96099d6c0f
YuyaSugimoto <yuya_fltteam7720@au.com>
2023-09-03 04:05:54 +0900

    update: スペースサービス、Fetchに使用するパラメーターを実装

--- domains/id-store/models/spaces/services/parameters/fetch_spaces_paramter.go
+++ domains/id-store/models/spaces/services/parameters/fetch_spaces_paramter.go
@@ -1,3 +1,50 @@
 package parameters
 
-type FetchSpacesParameter struct{}
+import "identity-management/domains/id-store/values/spaces"
+
+/*
+スペースを検索するためのパラメーター
+*/
+
+type FetchSpacesParameter struct {
+	spaceId   *spaces.SpaceId   // スペースID
+	nameSpace *spaces.NameSpace // スペース名
+}
+
+// FetchSpaceParameterBuilder フェッチスペースパラメータービルダー
+type FetchSpaceParameterBuilder struct {
+	fetchSpacesParameter *FetchSpacesParameter
+}
+
+// SpaceId スペースIDを検索パラメーターに設定します。
+func (f *FetchSpaceParameterBuilder) SpaceId(spaceId string) {
+	spaceIdParam, err := spaces.NewSpaceId(spaceId)
+	if err != nil {
+		println("スキップ: スペースIDに指定できない値が指定されました。")
+	}
+	f.fetchSpacesParameter.spaceId = spaceIdParam
+}
+
+// NameSpace ネームスペースを検索パラメータに設定します。
+func (f *FetchSpaceParameterBuilder) NameSpace(nameSpace string) {
+	nameSpaceParam, err := spaces.NewNameSpace(nameSpace)
+	if err != nil {
+		println("スキップ: スペース名に指定できない値が指定されました。")
+	}
+	f.fetchSpacesParameter.nameSpace = nameSpaceParam
+}
+
+func NewFetchSpaceParameterBuilder() FetchSpaceParameterBuilder {
+
+	return FetchSpaceParameterBuilder{
+		fetchSpacesParameter: new(FetchSpacesParameter),
+	}
+}
+
+func (f *FetchSpaceParameterBuilder) Build() FetchSpacesParameter {
+	if f.fetchSpacesParameter == nil {
+		println("FetchSpaceParameterBuilder: スペースの検索条件が指定されていません。")
+		return FetchSpacesParameter{}
+	}
+	return *f.fetchSpacesParameter
+}
