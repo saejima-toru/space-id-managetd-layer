76bd14fa5d92ea48aa4217e4bbf1d78fa6265955
YuyaSugimoto <yuya_fltteam7720@au.com>
2023-09-03 17:09:57 +0900

    update: AWS Cognito、ユーザープールの作成を実装

--- go.mod
+++ go.mod
@@ -6,3 +6,19 @@ require (
 	github.com/google/uuid v1.3.1
 	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63
 )
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.21.0 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.18.38 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.36 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.41 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.35 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.42 // indirect
+	github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider v1.26.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.35 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.13.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.15.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.21.5 // indirect
+	github.com/aws/smithy-go v1.14.2 // indirect
+)
--- infrastructure/spaces/aws_cognito_spaces_repository.go
+++ infrastructure/spaces/aws_cognito_spaces_repository.go
@@ -1,16 +1,48 @@
 package spaces
 
 import (
+	"context"
+	"errors"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
 	"identity-management/domains/id-store/models/spaces"
 	"identity-management/domains/id-store/models/spaces/services/parameters"
 )
 
 // AwsCognitoSpacesRepository AWS Cognitoを使用したスペースリポジトリ具象
-type AwsCognitoSpacesRepository struct{}
+type AwsCognitoSpacesRepository struct {
+	client *cognitoidentityprovider.Client
+}
 
+func NewAwsCognitoSpacesRepository() *AwsCognitoSpacesRepository {
+	cgf, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		println("Init -> NewAwsCognitoSpacesRepository: ", err.Error())
+	}
+
+	client := cognitoidentityprovider.NewFromConfig(cgf)
+	return &AwsCognitoSpacesRepository{
+		client: client,
+	}
+}
+
+// CreateSpaces スペースの永続化保存 (Space == UserPool)
 func (a *AwsCognitoSpacesRepository) CreateSpaces(createSpace spaces.Spaces) error {
-	//TODO implement me
-	panic("implement me")
+	castSpace, isSpace := createSpace.(spaces.Space)
+	if !isSpace {
+		return errors.New("スペースの永続化に失敗しました。")
+	}
+
+	nameSpace := castSpace.NameSpace()
+	createUserPoolInput := &cognitoidentityprovider.CreateUserPoolInput{
+		PoolName: aws.String(nameSpace.String()),
+	}
+
+	if _, createUserPoolErr := a.client.CreateUserPool(context.TODO(), createUserPoolInput); createUserPoolErr != nil {
+		return createUserPoolErr
+	}
+	return nil
 }
 
 func (a *AwsCognitoSpacesRepository) DeleteSpace(deleteSpace spaces.Spaces) error {
